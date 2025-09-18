5c6334a42e939fc120c7d1d27ab7d2decca467b5
icewithcola <me@kagurach.uk>
2025-09-18 14:18:11 +0800

    main-thread: init some helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use jni::{JavaVM, objects::JObject};
 use winit::{event_loop::EventLoop, platform::android::activity::AndroidApp};
 
+use crate::android::AndroidEnv;
+
+mod android;
 mod app;
 mod user_input;
 
@@ -7,7 +11,20 @@ mod user_input;
 fn android_main(app: AndroidApp) {
     use winit::platform::android::EventLoopBuilderExtAndroid;
 
-    android_logger::init_once(android_logger::Config::default().with_max_level(log::LevelFilter::Info));
-    let event_loop: EventLoop<AndroidApp> = EventLoop::<AndroidApp>::with_user_event().with_android_app(app).build().unwrap();
-    app::run(event_loop);
+    let vm = unsafe { JavaVM::from_raw(app.vm_as_ptr() as *mut _) };
+    let activity = unsafe { JObject::from_raw(app.activity_as_ptr() as *mut _) };
+    let android_env = if !(vm.is_err() || activity.is_null()) {
+        Some(AndroidEnv::new(vm.unwrap(), activity))
+    } else {
+        None
+    };
+
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
+    );
+    let event_loop: EventLoop<AndroidApp> = EventLoop::<AndroidApp>::with_user_event()
+        .with_android_app(app)
+        .build()
+        .unwrap();
+    app::run(event_loop, android_env);
 }
